6b17190433fbfe4c9eb212ce673f74edf9450dd3
Murilo Chaves Oliveira <murilochaves211105@gmail.com>
2025-08-27 17:47:24 -0300

    exercicio4

--- React/Ex-04/src/App.jsx
+++ React/Ex-04/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("GERAR");
+  const [copia, setCopia] = useState("Copiar");
+
+  function generatePassword(length) {
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let newPassword = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      newPassword += chars[randomIndex];
+    }
+    setText(newPassword);
+    setCopia("Copiar");
+    return newPassword;
+  }
+
+  async function copyToClipboard() {
+    if (text && text !== "GERAR") {
+      try {
+        await navigator.clipboard.writeText(text);
+        setCopia("Copiado! ✅"); // muda o botão
+      } catch (err) {
+        console.error("Erro ao copiar:", err);
+      }
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => generatePassword(10)}>Gerar Senha</button>
+        <button onClick={copyToClipboard}>{copia}</button>
+        <p>{text}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- React/Ex-04/src/main.jsx
+++ React/Ex-04/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1>Gerador de senhas</h1>
     <App />
   </StrictMode>,
 )
